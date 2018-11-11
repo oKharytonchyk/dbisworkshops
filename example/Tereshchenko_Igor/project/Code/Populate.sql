4c926ef74d23fcdc751d88df8157a87cb6f54839
Alexandr Kharytonchyk <ledoff.sky@ukr.net>
2018-11-11 21:02:48 +0200

    Update Populate.sql

--- example/Tereshchenko_Igor/project/Code/Populate.sql
+++ example/Tereshchenko_Igor/project/Code/Populate.sql
@@ -1,29 +1,14 @@
- 
-
 /* Create Users */
  
 INSERT INTO "User" (user_login, user_password, user_email) 
   VALUES ('ledoff.sky', 'qwerty123456', 'ledoff.sky@ukr.net');
- 
 INSERT INTO "User" (user_login, user_password, user_email) 
   VALUES ('Adamobskiy', 'lubliuPITON', 'Adamobskiy@gmail.com');
- 
 INSERT INTO "User" (user_login, user_password, user_email) 
   VALUES ('kotick', 'cockpit', 'hybris@gmail.com');
- 
 INSERT INTO "User" (user_login, user_password, user_email) 
   VALUES ('Slidan', 'youtube', 'slidan1@ukr.net');
 
- 
-/* Create Queues  */
-
-INSERT INTO Queue (room_number) VALUES (101);
-INSERT INTO Queue (room_number) VALUES (102);
-INSERT INTO Queue (room_number) VALUES (103);
-INSERT INTO Queue (room_number) VALUES (104);
-INSERT INTO Queue (room_number) VALUES (201);
-INSERT INTO Queue (room_number) VALUES (202);
- 
  
 /* Create Notifications */
 
@@ -35,23 +20,36 @@ INSERT INTO Notification (notification_status) VALUES ('You are next');
  
 /* Create Places */
 
-INSERT INTO Place (address, schedule) VALUES ('Bykova 11', '10:00 19:00');
-INSERT INTO Place (address, schedule) VALUES ('Bykova 73', '10:00 19:00');
-INSERT INTO Place (address, schedule) VALUES ('Virmenska 1', '10:00 19:00');
-INSERT INTO Place (address, schedule) VALUES ('Frunze 103', '09:00 18:00');
-INSERT INTO Place (address, schedule) VALUES ('Bazhana 7', '09:00 18:00');
+INSERT INTO Place (place_id, address, room_number, schedule) VALUES (1, 'Bykova 11', 101, '10:00 19:00');
+INSERT INTO Place (place_id, address, room_number, schedule) VALUES (2, 'Bykova 73', 101, '10:00 19:00');
+INSERT INTO Place (place_id, address, room_number, schedule) VALUES (3, 'Virmenska 1', 101, '10:00 19:00');
+INSERT INTO Place (place_id, address, room_number, schedule) VALUES (4, 'Frunze 103', 101, '09:00 18:00');
+INSERT INTO Place (place_id, address, room_number, schedule) VALUES (5, 'Bazhana 7', 101, '09:00 18:00');
+
+
+/* Create Events */
+
+INSERT INTO Event (event_name) VALUES ('Concert');
+INSERT INTO Event (event_name) VALUES ('Doctor');
+INSERT INTO Event (event_name) VALUES ('Lab work');
+INSERT INTO Event (event_name) VALUES ('Talent show');
  
  
-/* Create Users in queues for events */
+/* Create CreatedEvents */
 
-INSERT INTO UserInQueueForEvent(address, notification_status, room_number, user_login, create_request_date, wishlist_status)
-   VALUES ('Bykova 11', 'You are 10th', 102, 'ledoff.sky', TO_DATE('1976-10-09 07:59:33', 'YYYY-MM-DD HH24:MI:SS'), 'approved');
+INSERT INTO CreatedEvent (place_id, event_name, date_creation_event) VALUES (1, 'Concert', TO_DATE('2018-12-12', 'YYYY-MM-DD'));
+INSERT INTO CreatedEvent (place_id, event_name, date_creation_event) VALUES (2, 'Doctor', TO_DATE('2018-11-11', 'YYYY-MM-DD'));
+INSERT INTO CreatedEvent (place_id, event_name, date_creation_event) VALUES (3, 'Lab work', TO_DATE('2018-09-15', 'YYYY-MM-DD'));
+INSERT INTO CreatedEvent (place_id, event_name, date_creation_event) VALUES (4, 'Talent show', TO_DATE('2018-01-27', 'YYYY-MM-DD'));
 
-INSERT INTO UserInQueueForEvent(address, notification_status, room_number, user_login, create_request_date, wishlist_status)
-   VALUES ('Bykova 11', 'You are 10th', 102, 'ledoff.sky', TO_DATE('1976-10-10 08:59:33', 'YYYY-MM-DD HH24:MI:SS'), 'rejected');
 
-INSERT INTO UserInQueueForEvent(address, notification_status, room_number, user_login, create_request_date, wishlist_status)
-   VALUES ('Bykova 11', 'You are 5th', 102, 'ledoff.sky', TO_DATE('1976-10-11 09:59:33', 'YYYY-MM-DD HH24:MI:SS'), 'approved');
+/* Create Queues */
 
-INSERT INTO UserInQueueForEvent(address, notification_status, room_number, user_login, create_request_date, wishlist_status)
-   VALUES ('Bykova 73', 'You are next', 101, 'Slidan', TO_DATE('1976-10-12 17:25:11', 'YYYY-MM-DD HH24:MI:SS'), 'approved');
+INSERT INTO Queue (notification_status, user_login, place_id, event_name, date_creation_event, date_request_creation, wishlist_status)
+   VALUES ('You are 10th', 'ledoff.sky', 2, 'Doctor', TO_DATE('2018-11-11', 'YYYY-MM-DD'), TO_DATE('2019-01-01', 'YYYY-MM-DD'), 'approved');
+INSERT INTO Queue (notification_status, user_login, place_id, event_name, date_creation_event, date_request_creation, wishlist_status)
+   VALUES ('You are 10th', 'ledoff.sky', 2, 'Doctor', TO_DATE('2018-11-11', 'YYYY-MM-DD'), TO_DATE('2019-01-02', 'YYYY-MM-DD'), 'rejected');
+INSERT INTO Queue (notification_status, user_login, place_id, event_name, date_creation_event, date_request_creation, wishlist_status)
+   VALUES ('You are 3rd', 'Adamobskiy', 4, 'Talent show', TO_DATE('2018-01-27', 'YYYY-MM-DD'), TO_DATE('2019-01-03', 'YYYY-MM-DD'), 'approved');
+INSERT INTO Queue (notification_status, user_login, place_id, event_name, date_creation_event, date_request_creation, wishlist_status)
+   VALUES ('You are next', 'Slidan', 1, 'Concert', TO_DATE('2018-12-12', 'YYYY-MM-DD'), TO_DATE('2019-01-04', 'YYYY-MM-DD'), 'approved');
