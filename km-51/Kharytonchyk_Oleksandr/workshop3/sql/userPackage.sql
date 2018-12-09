4b971ea8483ae1c00a7440ee6cda0f09b59e2d14
Alexandr Kharytonchyk <ledoff.sky@ukr.net>
2018-12-09 21:57:41 +0200

    Update userPackage.sql

--- km-51/Kharytonchyk_Oleksandr/workshop3/sql/userPackage.sql
+++ km-51/Kharytonchyk_Oleksandr/workshop3/sql/userPackage.sql
@@ -1,44 +1,39 @@
 CREATE OR REPLACE PACKAGE USER_PACKAGE AS
 
-    TYPE T_USER IS RECORD (
-   	user_login VARCHAR2(20),
-  	user_password VARCHAR2(20),
- 	user_email VARCHAR2(40)
-    );
-    
-    TYPE T_USER_TABLE IS 
-    	TABLE OF T_USER;
-        
-    FUNCTION LOG_IN(LOGIN IN SuperUser.user_login%TYPE, 
-		    PASSWORD IN SuperUser.user_password%TYPE)
-    	RETURN T_USER_TABLE PIPELINED;
+  TYPE T_USER IS RECORD (
+  user_login VARCHAR2(20),
+  user_password VARCHAR2(20),
+  user_email VARCHAR2(40)
+  );
+
+  TYPE T_USER_TABLE IS
+    TABLE OF T_USER;
+
+  FUNCTION LOG_IN(LOGIN    IN SuperUser.user_login%TYPE,
+                  PASSWORD IN SuperUser.user_password%TYPE)
+    RETURN NUMBER;
 
   PROCEDURE REGISTER(LOGIN    IN SuperUser.user_login%TYPE,
-                     PASSWORD IN SuperUser.user_password %TYPE,
+                     PASSWORD IN SuperUser.user_password%TYPE,
                      EMAIL    IN SuperUser.user_email%TYPE);
 END;
 
 
-
 CREATE OR REPLACE PACKAGE BODY USER_PACKAGE AS
   FUNCTION LOG_IN(LOGIN IN SuperUser.user_login%TYPE, PASSWORD IN SuperUser.user_password%TYPE)
-    RETURN T_USER_TABLE PIPELINED AS
-    CURSOR MY_CUR IS
-      SELECT *
-      FROM SuperUser
-      WHERE user_login = LOGIN
-        AND user_password = PASSWORD;
-    BEGIN
-      FOR rec IN MY_CUR
-      LOOP
-        pipe row (rec);
-      end loop;
+    RETURN NUMBER AS
+    res NUMBER(1);
+    begin
+      SELECT count(*)
+          INTO res FROM SuperUser WHERE user_login = LOGIN
+                                    AND user_password = PASSWORD;
+      RETURN (res);
     END;
 
-  PROCEDURE REGISTER(LOGIN IN SuperUser.user_login%TYPE, PASSWORD IN SuperUser.user_password %TYPE,
+  PROCEDURE REGISTER(LOGIN IN SuperUser.user_login%TYPE, PASSWORD IN SuperUser.user_password%TYPE,
                      EMAIL IN SuperUser.user_email%TYPE) AS
     BEGIN
-      INSERT INTO SuperUser(user_login, user_password, user_email) VALUES (LOGIN, PASSWORD, EMAIL);
+      INSERT INTO SuperUser (user_login, user_password, user_email) VALUES (LOGIN, PASSWORD, EMAIL);
     END;
 
 END;
