f7eca2d3583125c62d2d6d1a82671cd33ee5ffef
Alexandr Kharytonchyk <ledoff.sky@ukr.net>
2018-12-22 10:47:48 +0200

    Update userPackage.sql

--- km-51/Kharytonchyk_Oleksandr/workshop3/sql/userPackage.sql
+++ km-51/Kharytonchyk_Oleksandr/workshop3/sql/userPackage.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE PACKAGE USER_PACKAGE AS
 
   TYPE T_USER IS RECORD (
-  user_login VARCHAR2(20),
-  user_password VARCHAR2(20),
-  user_email VARCHAR2(40)
+  user_login VARCHAR2(20)
+  --   user_password VARCHAR2(20),
+  --   user_email VARCHAR2(40)
   );
 
   TYPE T_USER_TABLE IS
@@ -17,6 +17,19 @@ CREATE OR REPLACE PACKAGE USER_PACKAGE AS
                     PASSWORD IN SuperUser.user_password%TYPE,
                     EMAIL    IN SuperUser.user_email%TYPE)
     RETURN VARCHAR2;
+
+  FUNCTION GET_USERS(LOGIN IN SuperUser.user_login%TYPE DEFAULT NULL)
+    RETURN T_USER_TABLE PIPELINED;
+
+  --   FUNCTION UPDATE_USERS(LOGIN    IN SuperUser.user_login%TYPE,
+  --                         PASSWORD IN SuperUser.user_password%TYPE,
+  --                         EMAIL    IN SuperUser.user_email%TYPE)
+  --     RETURN VARCHAR2;
+  --
+  --   FUNCTION DELETE_USERS(LOGIN    IN SuperUser.user_login%TYPE,
+  --                         PASSWORD IN SuperUser.user_password%TYPE,
+  --                         EMAIL    IN SuperUser.user_email%TYPE)
+  --     RETURN VARCHAR2;
 END;
 
 
@@ -50,9 +63,33 @@ CREATE OR REPLACE PACKAGE BODY USER_PACKAGE AS
         STATUS := SQLERRM;
     END;
 
+  FUNCTION GET_USERS(LOGIN IN SuperUser.user_login%TYPE DEFAULT NULL)
+    RETURN T_USER_TABLE
+  PIPELINED
+  IS
+    TYPE user_cursor_type IS REF CURSOR;
+    user_cursor user_cursor_type;
+    cursor_data T_USER;
+    query_str   varchar2(1000);
+    begin
+      query_str := 'select user_login
+                        from SUPERUSER';
+      if LOGIN is not null
+      then
+        query_str := query_str || ' where trim(user_login) = trim(''' || LOGIN || ''') ';
+      end if;
+      query_str := query_str || ' group by user_login';
+
+      OPEN user_cursor FOR query_str;
+      LOOP
+        FETCH user_cursor into cursor_data;
+        exit when (user_cursor%NOTFOUND);
+        PIPE ROW (cursor_data);
+      END LOOP;
+    END GET_USERS;
 
 END USER_PACKAGE;
 /
 
-select USER_PACKAGE.LOG_IN('ledoff.sky', 'qwerty123456')
-from dual;
+select *
+from table (USER_PACKAGE.GET_USERS());
